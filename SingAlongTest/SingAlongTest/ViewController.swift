3a9fe9f2fe6a4db39e1479af5e20651ea1154b89
Adnan Alkallas <adnanta@kth.se>
2019-12-16 21:55:24 +0100

    New Test App

---
+++ SingAlongTest/SingAlongTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingAlongTest
+//
+//  Created by Adnan Kallas on 2019-12-16.
+//  Copyright © 2019 Adnan ALKallas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
