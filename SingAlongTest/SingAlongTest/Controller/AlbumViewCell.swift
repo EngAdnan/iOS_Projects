56f9c29c3056220da695fc5c04a5036fecb394cc
Adnan Alkallas <adnanta@kth.se>
2019-12-19 15:19:43 +0100

    Show List of Albums

---
+++ SingAlongTest/SingAlongTest/Controller/AlbumViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumViewCell.swift
+//  SingAlongTest
+//
+//  Created by Adnan Kallas on 2019-12-19.
+//  Copyright © 2019 Adnan ALKallas. All rights reserved.
+//
+
+import UIKit
+
+class AlbumViewCell: UICollectionViewCell {
+    
+}
